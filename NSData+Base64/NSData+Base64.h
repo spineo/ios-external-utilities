53b5d626c8fe8fde9582cce7dbedd68d7cd627d2
Stuart Pineo <svpineo@gmail.com>
2017-02-04 08:54:05 -0500

    Commented out methods in NSData+Base64 that resulted in warning and not being used.

--- NSData+Base64/NSData+Base64.h
+++ NSData+Base64/NSData+Base64.h
@@ -8,14 +8,14 @@
  contents of the Base 64 encoded string. This is a convenience method.
 	@param	inBase64String	An NSString object that contains only Base 64 encoded data.
 	@result	The NSData object. */
-+ (NSData *) dataWithBase64EncodedString:(NSString *) string;
+//+ (NSData *) dataWithBase64EncodedString:(NSString *) string;
 
 /*!	@function	-initWithBase64EncodedString:
 	@discussion	The NSData object is initialized with the contents of the Base 64 encoded string.
  This method returns self as a convenience.
 	@param	inBase64String	An NSString object that contains only Base 64 encoded data.
 	@result	This method returns self. */
-- (id) initWithBase64EncodedString:(NSString *) string;
+//- (id) initWithBase64EncodedString:(NSString *) string;
 
 /*!	@function	-base64EncodingWithLineLength:
 	@discussion	This method returns a Base 64 encoded string representation of the data object.
